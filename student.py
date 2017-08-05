7b668d2b0ce81a995b485f20688d548f04b7b5c2
sylwia <zaleska.syl@gmail.com>
2017-08-05 20:48:19 +0200

    Add __gt__ method for get average grade

--- student.py
+++ student.py
@@ -9,8 +9,12 @@ class Student(Person):
 
     def get_average_grade(self):
         grade = 0
-        for grade in self.grades:
-            grade += grade
+        for g in range(len(self.grades)):
+            grade += self.grades[g]
         avg_grade = grade/len(self.grades)
 
         return avg_grade
+
+    def __gt__(self, other):
+        return self.get_average_grade() > other.get_average_grade()
+
