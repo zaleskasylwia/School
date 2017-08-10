7bcd4f0749a06586b5840a66e6eaed8514c2a8c0
sylwia <zaleska.syl@gmail.com>
2017-08-10 07:52:28 +0200

    Change checking word in init method for str not string

--- teacher.py
+++ teacher.py
@@ -4,7 +4,7 @@ from person import Person
 class Teacher(Person):
 
     def __init__(self, first_name, last_name, subjects=[]):
-        if type(first_name) is string and type(last_name) is string and type(subjects) is list:
+        if type(first_name) is str and type(last_name) is str and type(subjects) is list:
             super().__init__(first_name, last_name)
             self.subjects = subjects
         else:
